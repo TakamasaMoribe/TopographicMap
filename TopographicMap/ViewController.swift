5d8a745c644408ad8d9493db8fe577f490d6d0ca
森部高昌 <takamasamoribe@gmail.com>
2021-09-25 20:15:14 +0900

    0925  1  とりあえずコードをコピーして動かしてみた

--- TopographicMap/ViewController.swift
+++ TopographicMap/ViewController.swift
@@ -5,15 +5,35 @@
 //  Created by 森部高昌 on 2021/09/25.
 //
 
+
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+
+    @IBAction func sliderDidChange(_ slider: UISlider) {
+        if let renderer = mapView.renderer(for: tileOverlay) {
+            renderer.alpha = CGFloat(slider.value)
+        }
+    }
+    
+    // 国土地理院が提供する色別標高図のURL
+    // ここを変えるだけで、様々な地図データを表示できる！
+    private let tileOverlay = MKTileOverlay(urlTemplate: "https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png")
+    //https://cyberjapandata.gsi.go.jp/xyz/relief/{z}/{x}/{y}.png
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        mapView.delegate = self
+        mapView.addOverlay(tileOverlay, level: .aboveLabels)
     }
-
-
 }
 
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        return MKTileOverlayRenderer(overlay: overlay)
+    }
+}
